7b140c67e9214e669f78d277ff131f8e6aa5ec1d
francois <francoisrvespa@gmail.com>
2015-05-16 18:27:44 +0200

    Babel: modules to common

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
   
   'use strict';
 
-  require("babel/register")({});
+  require("babel/register")({ modules: 'common' });
 
   exports.Element     =   require('./Element');
   exports.Elements    =   require('./Elements');
   exports.Document    =   require('./Document');
   exports.toHTML      =   require('./toHTML');
+  exports.render      =   require('./toHTML');
 
 } ();
