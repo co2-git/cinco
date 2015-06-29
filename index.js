fce8ac10cdfbbba7f032f2f8473f07ff827d2480
francois <francoisrvespa@gmail.com>
2015-06-29 14:37:21 +0700

    dist files transpiled JS NExt+

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import Element from './Element';
-import Elements from './Elements';
-import Document from './Document';
-import Compiler from './Compiler';
+import Element    from './lib/element';
+import Elements   from './lib/elements';
+import Document   from './lib/document';
+import Compiler   from './lib/compiler';
 
 function Cinco () {
   this.Element     =   Element;
